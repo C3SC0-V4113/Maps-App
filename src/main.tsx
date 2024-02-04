96a65c06978671d70a87f3566bf52ae4f6fb4a25
CESCO <cescovalledev@gmail.com>
2024-02-03 22:40:29 -0600

    MapView with env added

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { MapsApp } from "./MapsApp";
+import mapboxgl from "mapbox-gl";
+
 import "./styles.css";
+mapboxgl.accessToken = import.meta.env.VITE_MAPBOX;
 
 if (!navigator.geolocation) {
   alert("Tu navegador no tiene acceso a la geolocalización");
