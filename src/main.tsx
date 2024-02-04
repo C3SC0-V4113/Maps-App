3025c0cb58139819ba388fc282801ed187ed9464
CESCO <cescovalledev@gmail.com>
2024-02-03 21:49:19 -0600

    getUserLocation

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { MapsApp } from "./MapsApp";
 
+if (!navigator.geolocation) {
+  alert("Tu navegador no tiene acceso a la geolocalización");
+  throw new Error("Tu navegador no tiene acceso a la geolocalization");
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <MapsApp />
