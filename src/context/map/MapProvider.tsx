07b444b03c30137b8d384b168b90256aa630bfb8
CESCO <cescovalledev@gmail.com>
2024-02-03 23:12:53 -0600

    Creating map Provider

---
+++ src/context/map/MapProvider.tsx
@@ -0,0 +1,25 @@
+import { useReducer } from "react";
+import { mapReducer } from "./mapReducer";
+import { MapContext } from "./MapContext";
+import { Map } from "mapbox-gl";
+
+export interface MapState {
+  isMapReady: boolean;
+  map?: Map;
+}
+
+const INITIAL_STATE: MapState = {
+  isMapReady: false,
+};
+
+interface Props {
+  children: JSX.Element | JSX.Element[];
+}
+
+export const MapProvider = ({ children }: Props) => {
+  const [state, dispatch] = useReducer(mapReducer, INITIAL_STATE);
+
+  return (
+    <MapContext.Provider value={{ ...state }}>{children}</MapContext.Provider>
+  );
+};
