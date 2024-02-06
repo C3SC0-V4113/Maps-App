7efbb93e831d65f7e3aa52795578ee5ff5f79741
CESCO <cescovalledev@gmail.com>
2024-02-05 22:21:11 -0600

    Added Polylines

--- src/context/map/MapProvider.tsx
+++ src/context/map/MapProvider.tsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useReducer } from "react";
 import { mapReducer } from "./mapReducer";
 import { MapContext } from "./MapContext";
-import { Map, Marker, Popup } from "mapbox-gl";
+import { AnySourceData, LngLatBounds, Map, Marker, Popup } from "mapbox-gl";
 import { PlacesContext } from "..";
 import { directionsApi } from "../../apis";
 import { DirectionsResponse } from "../../interfaces/directions";
@@ -73,6 +73,9 @@ export const MapProvider = ({ children }: Props) => {
     );
 
     const { distance, duration, geometry } = resp.data.routes[0];
+
+    const { coordinates: coords } = geometry;
+
     let kms = distance / 1000;
     kms = Math.round(kms * 1000);
     kms /= 100;
@@ -80,6 +83,56 @@ export const MapProvider = ({ children }: Props) => {
     const minutes = Math.floor(duration / 60);
 
     console.log(kms, minutes);
+
+    const bounds = new LngLatBounds(start, start);
+
+    for (const coord of coords) {
+      const newCoord: [number, number] = [coord[0], coord[1]];
+      bounds.extend(newCoord);
+    }
+
+    state.map?.fitBounds(bounds, {
+      padding: 200,
+    });
+
+    /** Polyline */
+    const sourceData: AnySourceData = {
+      type: "geojson",
+      data: {
+        type: "FeatureCollection",
+        features: [
+          {
+            type: "Feature",
+            properties: {},
+            geometry: {
+              type: "LineString",
+              coordinates: coords,
+            },
+          },
+        ],
+      },
+    };
+
+    if (state.map?.getLayer("RouteString")) {
+      state.map.removeLayer("RouteString");
+      state.map.removeSource("RouteString");
+    }
+
+    state.map?.addSource("RouteString", sourceData);
+
+    state.map?.addLayer({
+      id: "RouteString",
+      type: "line",
+      source: "RouteString",
+      layout: {
+        "line-cap": "round",
+        "line-join": "round",
+      },
+      paint: {
+        "line-color": "black",
+        "line-width": 3,
+      },
+    });
   };
 
   return (
